2c840d4ad3c5b776f34b3db20f7a5794e5f24c0b
HaiQW <HaiQW@users.noreply.github.com>
2016-04-13 21:15:02 -0500

    Delete main.py

--- main.py
+++
@@ -1,9 +0,0 @@
-#-*- coding:utf-8 -*-
-__requires__ = 'Scrapy==0.24.4'
-from pkg_resources import load_entry_point
-
-if __name__ == '__main__':
-    print "run."
-    # weibo_argv = ["scrapy", "crawl", "weibo", "-a", "name=15557106533", "-a", "password=87092046", "-a", "uid=3856926178"]
-    zhihu_argv = ["scrapy", "crawl", "zhihu_spider", "-a", "name=wenghaiqin@qq.com", "-a", "password=87092046", "-a", "uid=3856926178"]
-    load_entry_point('Scrapy==0.24.4', 'console_scripts', 'scrapy')(zhihu_argv)
