d1694bd1418268dc69c7dcc8a7d59a5e29174eb5
HaiQW <hq_weng@zju.edu.cn>
2016-04-18 14:44:46 +0800

    修改zhihu爬虫

--- webspiders/spiders/zhihu_spider.py
+++ webspiders/spiders/zhihu_spider.py
@@ -8,11 +8,13 @@ import logging.config
 
 from scrapy.http import Request
 from scrapy.spider import Spider
+from scrapy.http import TextResponse
 
 from selenium import webdriver
 from selenium.webdriver.common.by import By
 from selenium.webdriver.support.wait import WebDriverWait
 from selenium.webdriver.support import expected_conditions as EC
+from selenium.webdriver.support.ui import WebDriverWait
 
 from webspiders.utils.zhihu_login import ZhihuLogin
 # from webspiders.items import WeiboItem, ParamItem
@@ -33,30 +35,31 @@ class ZhihuSpider(Spider):
         self.start_urls = ['http://m.zhihu.com']
         self.allowed_domains = ['zhihu.com']
         self.zhihu = ZhihuLogin(name, password)
-        self.session = self.zhihu.login()  # zhihu模拟登陆
-        cookiejar = requests.utils.dict_from_cookiejar(self.session.cookies)
-        self.cookies = {'q_c1': cookiejar['q_c1'],
-                        'z_c0': cookiejar['z_c0'],
-                        'cap_id': cookiejar['cap_id'],
-                        '_xsrf': cookiejar['_xsrf'],
-                        'unlock_ticket': cookiejar['unlock_ticket'],
-                        'l_cap_id': cookiejar['l_cap_id'],
-                        'login': cookiejar['login'],
-                        'n_c': cookiejar['n_c']}
-        self.cookies2 = {'name': 'q_c1', 'value': cookiejar['q_c1'],
-                         'name': 'z_c0', 'value': cookiejar['z_c0'],
-                         'name': 'cap_id', 'value': cookiejar['cap_id'],
-                         'name': '_xsrf', 'value': cookiejar['_xsrf'],
-                         'name': 'unlock_ticket', 'value': cookiejar['unlock_ticket'],
-                         'name': 'l_cap_id', 'value': cookiejar['l_cap_id'],
-                         'name': 'login', 'value': cookiejar['login'],
-                         'name': 'n_c', 'value': cookiejar['n_c']}
-        self.headers = {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64)'
-                                      'AppleWebKit/537.36(KHTML, like Gecko)'
-                                      'Chrome/44.0.2403.155 Safari/537.36',
-                        'Origin': 'Origin: https://m.zhihu.com',
-                        'Content-Type': 'application/x-protobuf'}
+        # self.session = self.zhihu.login()  # zhihu模拟登陆
+        #cookiejar = requests.utils.dict_from_cookiejar(self.session.cookies)
+        #self.cookies = {'q_c1': cookiejar['q_c1'],
+        #                'z_c0': cookiejar['z_c0'],
+        #                'cap_id': cookiejar['cap_id'],
+        #                '_xsrf': cookiejar['_xsrf'],
+        #                'unlock_ticket': cookiejar['unlock_ticket'],
+        #                'l_cap_id': cookiejar['l_cap_id'],
+        #                'login': cookiejar['login'],
+        #                'n_c': cookiejar['n_c']}
+        #self.cookies2 = {'name': 'q_c1', 'value': cookiejar['q_c1'],
+        #                 'name': 'z_c0', 'value': cookiejar['z_c0'],
+        #                 'name': 'cap_id', 'value': cookiejar['cap_id'],
+        #                 'name': '_xsrf', 'value': cookiejar['_xsrf'],
+        #                 'name': 'unlock_ticket', 'value': cookiejar['unlock_ticket'],
+        #                 'name': 'l_cap_id', 'value': cookiejar['l_cap_id'],
+        #                 'name': 'login', 'value': cookiejar['login'],
+        #                 'name': 'n_c', 'value': cookiejar['n_c']}
+        #self.headers = {'User-agent': 'Mozilla/5.0 (X11; Linux x86_64)'
+                        #              'AppleWebKit/537.36(KHTML, like Gecko)'
+                        #              'Chrome/44.0.2403.155 Safari/537.36',
+                        #'Origin': 'Origin: https://m.zhihu.com',
+                        #'Content-Type': 'application/x-protobuf'}
         self.driver = webdriver.Firefox()
+        self._login()
         # self.driver.add_cookie(self.cookies)
 
     def start_requests(self):
@@ -64,8 +67,8 @@ class ZhihuSpider(Spider):
         home_url = 'http://m.zhihu.com/'
         questions_url = 'https://m.zhihu.com/log/questions'
         # print self.driver.get_cookies()
-        self.driver.get(home_url)
-        self.driver.add_cookie(self.cookies2)
+        # self.driver.get(home_url)
+        # self.driver.add_cookie(self.cookies2)
         # print self.driver.get_cookies()
         # self.driver.close()
         yield Request(url=home_url, cookies=self.cookies, headers=self.headers,
@@ -86,10 +89,25 @@ class ZhihuSpider(Spider):
 
     def _login(self):
         """知乎爬虫的登陆模块(此处用selenium模拟登陆)"""
-       r = 1
-       while r != 0:
-           print(u'输入用户名:')
-           self.user = raw_input()
-           print(u'输入密码:')
-           self.pwd = raw_input()
-           self.
+        r = 1
+        while r != 0:
+            # try:
+                self.driver.set_page_load_timeout(20)  # 防止页面加载不完
+                self.driver.get('https://www.zhihu.com/#signin')
+                time.sleep(10)  # sleep 20 秒等待用户输入账号信息
+                response = TextResponse(u'https://www.zhihu.com/#signin', self.driver.page_source, encoding='utf-8')
+                response.xpath('//script')
+            #except:
+            #    pass
+            # email = self.driver.find_element_by_xpath("//input[@name='email']")
+            # email.clear()
+            # email.send_keys(self.user)
+            # password = self.driver.find_element_by_xpath("//input[@name='password']")
+            # password.clear()
+            # password.send_keys(self.pwd)
+            # form = self.driver.find_element_by_xpath("//form[@class='zu-side-login-box']")
+            # form.submit()
+            # somedom = WebDriverWait(self.driver, 60).until(lambda brow: brow.find_elements_by_class_name("zu-main-feed-con"))[0]
+            # html = somedom.find_element_by_xpath("//*").get_attribute("outerHTML")
+            # print html
+            # self.driver.quit()
